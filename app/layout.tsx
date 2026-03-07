2c0b2c383a0bb0c7a6cbc6a22ff6527b467d7760
devChris11 <43631204+devChris11@users.noreply.github.com>
2026-03-07 10:05:43 +0200

    07-03-2026:Updated Design System

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lexend, Lexend_Exa } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const lexend = Lexend({
+  variable: "--font-lexend",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const lexendExa = Lexend_Exa({
+  variable: "--font-lexend-exa",
   subsets: ["latin"],
 });
 
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lexend.variable} ${lexendExa.variable} antialiased`}
       >
         {children}
       </body>
